cc403cd80b2662a0743d2a02161a36009917dca4
Marcin Fortuna <marcin.fort@gmail.com>
2017-11-26 13:17:01 +0100

    Add basic registration and login forms

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <div id="rejestracja" style={{float:'left'}}>
+            REJESTRACJA
+            <form action="#">
+              Imię: <input type="text" name="fname" /> <br />
+              Nazwisko: <input type="text" name="lname" /> <br />
+              E-mail: <input type="text" name="email" /> <br />
+              Login: <input type="text" name="login" /> <br />
+              Hasło: <input type="text" name="password" /> <br />
+              <input type="submit" value="Wyślij" /> <br />
+            </form>
+          </div>
+          <div id="logowanie" style={{float:'left'}}>
+          LOGOWANIE
+          <form action="#">
+              Login: <input type="text" name="login" /> <br />
+              Hasło: <input type="text" name="password" /> <br />
+              <input type="submit" value="Wyślij" /> <br />
+          </form>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
+
   }
 }
 
