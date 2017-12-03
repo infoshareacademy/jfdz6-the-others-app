c349491f5d8253a71811deb1364af3cfe1992f10
Marcin Fortuna <marcin.fort@gmail.com>
2017-12-03 13:48:44 +0100

    Add basic formatting

--- src/Login.js
+++ src/Login.js
@@ -18,9 +18,23 @@ class Login extends Component {
   render() {
     return (
       <div id="signup-login">
-        Nie jesteś zalogowany!
-        <header>
-          <div className="signup" style={{float:'right'}}>
+        <div id="noLogin">
+          Witaj!
+          <br />
+          Nie jesteś zalogowany!
+          <br />
+          Aby korzystać z aplikacji, zaloguj się lub zarejestruj.
+        </div>
+        <div>
+          <div className="login">
+            <span className="login" onClick={this.toggleForm} data-form-id="showSignUpForm">LOGOWANIE</span>
+            <form action="#" className={'login ' + (this.state.showSignUpForm ? 'formvisible' : 'formhidden')}>
+              Login: <input type="text" name="login" /> <br />
+              Hasło: <input type="text" name="password" /> <br />
+              <input type="submit" value="Wyślij" /> <br />
+            </form>
+          </div>
+          <div className="signup">
             <span className="signup" onClick={this.toggleForm} data-form-id="showLoginForm">REJESTRACJA</span>
             <form action="#" className={'signup ' + (this.state.showLoginForm ? 'formvisible' : 'formhidden')}>
               Imię: <input type="text" name="fname" /> <br />
@@ -31,15 +45,7 @@ class Login extends Component {
               <input type="submit" value="Wyślij" /> <br />
             </form>
           </div>
-          <div className="login" style={{float:'right'}}>
-            <span className="login" onClick={this.toggleForm} data-form-id="showSignUpForm">LOGOWANIE</span>
-            <form action="#" className={'login ' + (this.state.showSignUpForm ? 'formvisible' : 'formhidden')}>
-              Login: <input type="text" name="login" /> <br />
-              Hasło: <input type="text" name="password" /> <br />
-              <input type="submit" value="Wyślij" /> <br />
-            </form>
-          </div>
-        </header>
+        </div>
 
       </div>
     );
