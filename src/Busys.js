9a3e2e71f88c32ebc781f2408f7aedab3b551f5f
Piotr Barcik <piotrbarcik83@gmail.com>
2017-12-16 19:38:14 +0100

    Add fetch.

--- src/Busys.js
+++ src/Busys.js
@@ -1,21 +1,24 @@
 import React from 'react';
 
-export default class Busys extends React.Component{
+export default class Busys extends React.Component {
 
-  constructor(props){
-    let routes = []
-    super(props)
-    fetch(
-      'http://91.244.248.19/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/4128329f-5adb-4082-b326-6e1aea7caddf/download/routes.json', { method: 'get', mode: 'no-cors', }
-    ).then(
-      response => routes = response.json()
-    ).then(
-      json => console.log(json)
-    ).catch(console.log)
-
-  }
-
-  //routeShortName routeLongName activationDate
+    constructor(props) {
+        let routes = []
+        super(props)
+        fetch(
+            'http://91.244.248.19/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/4128329f-5adb-4082-b326-6e1aea7caddf/download/routes.json', {
+                method: 'get',
+                mode: 'no-cors',
+            })
+            .then(
+                response =>array.filter(function() {
+                    return routeShortName, routeLongName, activationDate = response.json()
+                })).then(
+            response => routes = response.json()
+        ).then(
+            json => console.log(json)
+        ).catch(console.log)
+    }
 
   render(){
 
