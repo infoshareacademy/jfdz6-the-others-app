f9737d24b271e4f9658d4ca0d3bc530eb777e91a
Marcin Fortuna <marcin.fort@gmail.com>
2017-12-17 13:51:27 +0100

    Add a background for the login page

--- src/Auth.js
+++ src/Auth.js
@@ -11,17 +11,19 @@ class Auth extends Component {
       <div>
         {
           this.props.auth.data === null ?
-            <div id="signup-login">
-              <div id="noLogin">
-                Witaj!
-                <br/>
-                Nie jesteś zalogowany!
-                <br/>
-                Aby korzystać z aplikacji, zaloguj się lub zarejestruj.
-              </div>
-              <div>
-                <Login/>
-                <SignUp/>
+            <div id="scroll-bg">
+              <div id="signup-login">
+                <div id="noLogin">
+                  Witaj!
+                  <br/>
+                  Nie jesteś zalogowany!
+                  <br/>
+                  Aby korzystać z aplikacji, zaloguj się lub zarejestruj.
+                </div>
+                <div>
+                  <Login/>
+                  <SignUp/>
+                </div>
               </div>
             </div>
             :
