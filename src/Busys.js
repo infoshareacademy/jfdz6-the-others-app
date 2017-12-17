bf143593ea55967d1415f62dfaa4084fd1f03c14
Piotr Barcik <piotrbarcik83@gmail.com>
2017-12-17 13:11:43 +0100

    New data about busys stop.

--- src/Busys.js
+++ src/Busys.js
@@ -1,29 +1,51 @@
 import React from 'react';
 
+const ADD_BUS_STOP = 'Busys/ADD_BUS_STOP'
+
 export default class Busys extends React.Component {
 
-    constructor(props) {
-        let routes = []
-        super(props)
-        fetch(
-            'http://91.244.248.19/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/4128329f-5adb-4082-b326-6e1aea7caddf/download/routes.json', {
-                method: 'get',
-                mode: 'no-cors',
-            })
-            .then(
-                response =>array.filter(function() {
-                    return routeShortName, routeLongName, activationDate = response.json()
-                })).then(
-            response => routes = response.json()
-        ).then(
-            json => console.log(json)
-        ).catch(console.log)
-    }
+  constructor(props) {
+    super(props)
+    fetch(
+      'http://91.244.248.19/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/4128329f-5adb-4082-b326-6e1aea7caddf/download/routes.json'
+    ).then(
+      response => response.json()
+    ).then(
+      resp =>Object.values(resp).map(
+          ({ lastUpdate, routes }) => ({
+            lastUpdate,
+            routes: routes.map(
+              ({ routeShortName, routeLongName, activationDate }) => ({ routeShortName, routeLongName, activationDate })
+            )
+          })
+        )
+    ).then(
+      data => dispatch({ type: ADD_BUS_STOP, data })
+    ).catch(
+      error => console.warn(error)
+    )
+  }
 
-  render(){
+  render() {
 
     return (
       <h2>aaa</h2>
     )
+  }
+}
+
+const initialState = {
+  data: null
+}
+
+export default (state = initialState, action = {}) => {
+  switch (action.type) {
+  case ADD_BUS_STOP:
+  return {
+    ...state,
+    data: action.data,
+   }
+    default:
+      return state
   }
 }
