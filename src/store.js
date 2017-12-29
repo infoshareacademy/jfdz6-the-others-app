bb2fcae18e4f813239f7d45f5d2686c9f13dd76a
Bartosz Cytrowski <bartosz.cytrowski@gmail.com>
2017-12-29 23:47:52 +0100

    cleanup :)

--- src/store.js
+++ src/store.js
@@ -3,6 +3,7 @@ import persistState from 'redux-localstorage'
 import thunk from 'redux-thunk'
 import firebase from 'firebase'
 import auth from './state/auth'
+import buses from './state/buses'
 
 
 // Initialize Firebase
@@ -19,7 +20,7 @@ firebase.initializeApp(config);
 
 const reducer = combineReducers({
   auth,
-  data:[]
+  buses
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
