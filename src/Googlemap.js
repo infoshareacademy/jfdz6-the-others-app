e6efbb3f0d8d6b1b5de5b316f26d6d61c3b5b221
Marcin Fortuna <marcin.fort@gmail.com>
2018-01-14 13:54:40 +0100

    Update GoogleMap

--- src/Googlemap.js
+++ src/Googlemap.js
@@ -2,6 +2,7 @@ import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import {connect} from 'react-redux'
 import googleMap from './state/googlemap';
+import { Stops } from './Stops';
 
 
 export class GoogleMap extends React.Component {
@@ -20,6 +21,9 @@ export class GoogleMap extends React.Component {
         defaultCenter={this.props.center}
         defaultZoom={this.props.zoom}
       >
+        <Stops {...this.props.stop1coords} text={'A'} />
+        <Stops {...this.props.stop2coords} text={'B'} />
+
       </GoogleMapReact>
     );
   }
@@ -27,7 +31,7 @@ export class GoogleMap extends React.Component {
 
 const mapStateToProps = state => ({
   googlemap: state.googlemap
-})
+});
 
 export default connect(
   mapStateToProps,
--- src/state/googlemap.js
+++ src/state/googlemap.js
@@ -1,6 +1,8 @@
 const initialState = {
   center: {lat: 54.3520, lng: 18.6466},
-  zoom: 11
+  zoom: 11,
+  stop1coords: {lat: 54.3500, lng: 18.6465},
+  stop2coords: {lat: 54.24356, lng: 18.33598}
 };
 
 export default initialState
